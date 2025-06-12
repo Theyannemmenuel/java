e9179e8368bda28d6f04c2dad0f5ffe82bfbdabd
Yann Amangoua <yannamangoua31@gmail.com>
2025-06-12 14:08:12 +0200

    tp 1 suite weapon

--- hello-player/Player.java
+++ hello-player/Player.java
@@ -1,10 +1,12 @@
+import java.util.Random;
+
 public class Player {
     private final String name;
-    private int hp;  // Points de vie
+    private int hp;
 
     public Player(String name) {
         this.name = name;
-        this.hp = 100; // Initialisation des HP
+        this.hp = 100;
     }
 
     public void greet() {
@@ -13,7 +15,7 @@ public class Player {
 
     public void takeDamage(int amount) {
         hp -= amount;
-        if (hp < 0) hp = 0; // Pour éviter les HP négatifs
+        if (hp < 0) hp = 0;
         System.out.println(name + " a subi " + amount + " dégâts. HP restants : " + hp);
     }
 
@@ -21,20 +23,40 @@ public class Player {
         return hp > 0;
     }
 
+    public int getHp() {
+        return hp;
+    }
+
+    // j'ai crée une classe weapon directement dansle meme fichier 
+    public static class Weapon {
+        private final int minDamage;
+        private final int maxDamage;
+        private final Random rand = new Random();
+
+        public Weapon(int minDamage, int maxDamage) {
+            this.minDamage = minDamage;
+            this.maxDamage = maxDamage;
+        }
+
+        public int rollDamage() {
+            return rand.nextInt(maxDamage - minDamage + 1) + minDamage;
+        }
+    }
+
     public static void main(String[] args) {
         Player yann = new Player("Yann");
-    
-        yann.takeDamage(10); // Yann subit tous les dégâts d'un coup
-    
-        // Teste s'il est encore en vie
+        Weapon sword = new Weapon(10, 25);
+
+        yann.greet();
+
+        int damage = sword.rollDamage();
+        System.out.println("L'arme inflige " + damage + " dégâts.");
+        yann.takeDamage(damage);
+
         if (yann.isAlive()) {
-            System.out.println("Yann est encore en vie !");
+            System.out.println(yann.name + " est encore en vie avec " + yann.getHp() + " HP !");
         } else {
-            System.out.println("Yann est KO !");
+            System.out.println(yann.name + " est KO !");
         }
-    
-        // Assertion pour les tests automatiques
-        assert !yann.isAlive();
     }
-    
 }
